0e91dbb8201fc19328afc8fe52f657c9651df46c
serhat-ismail <serhat.akkus@zuehlke.com>
2025-04-16 18:45:29 +0200

    new images

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <p className="pokemon-title">Pokemon</p>
         {children}
       </body>
     </html>
